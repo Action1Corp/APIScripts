ff9871dbfef20a0575624613dfe8ed69125735fc
Sean-Carroll-Action1 <147112281+Sean-Carroll-Action1@users.noreply.github.com>
2024-01-08 16:41:10 -0600

    Update PSAction1ExportReportFromAllOrganizations.ps1

--- PSAction1ExportReportFromAllOrganizations.ps1
+++ PSAction1ExportReportFromAllOrganizations.ps1
@@ -1,6 +1,6 @@
 # The script provided below is an example script. Carefully read it and consider executing it on a test enviroment first.
 # Action1 Corporation holds no liability for any damages directly or indirectly caused by running this script.
-# script is designed to generate a unique CSV file for each organization, allows for quick reporting across all orgs.
+# script is designed to generate a unique CSV file for each organization, allows for quick reporting across all orgs.  Requires Enterprise Admin rights via API Key to work across all organizations. 
 # Report ID can be found in the URL of the report that you are planning to export. example:  https://app.action1.com/console/reports/installed_software_1635264799139/summary?details=no&from=0&limit=50&live_only=no&org=ae4c8042-5a59-c0a4-1499-421f94b9c797 - installed_software_1635264799139 is the report id
 
 # Import the PSAction1 module
