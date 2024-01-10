887a4f2ab434df6699951a48a3fe4197cb9fd2f3
Sean-Carroll-Action1 <147112281+Sean-Carroll-Action1@users.noreply.github.com>
2024-01-10 17:21:51 -0600

    Update PSAction1ExportReportFromAllOrganizations.ps1

--- PSAction1ExportReportFromAllOrganizations.ps1
+++ PSAction1ExportReportFromAllOrganizations.ps1
@@ -1,7 +1,22 @@
-# The script provided below is an example script. Carefully read it and consider executing it on a test enviroment first.
-# Action1 Corporation holds no liability for any damages directly or indirectly caused by running this script.
-# script is designed to generate a unique CSV file for each organization, allows for quick reporting across all orgs.  Requires Enterprise Admin rights via API Key to work across all organizations. 
+﻿# Name: PSAction1ExportReportFromAllOrganizations.ps1
+# Description: script is designed to generate a unique CSV file for each organization, allows for quick reporting across all orgs.  Requires Enterprise Admin rights via API Key to work across all organizations. 
 # Report ID can be found in the URL of the report that you are planning to export. example:  https://app.action1.com/console/reports/installed_software_1635264799139/summary?details=no&from=0&limit=50&live_only=no&org=ae4c8042-5a59-c0a4-1499-421f94b9c797 - installed_software_1635264799139 is the report id
+# Copyright (C) 2024 Action1 Corporation
+# Documentation: https://github.com/Action1Corp/PSAction1/
+# Use Action1 Roadmap system (https://roadmap.action1.com/) to submit feedback or enhancement requests.
+
+# WARNING: Carefully study the provided scripts and components before using them. Test in your non-production lab first.
+
+# LIMITATION OF LIABILITY. IN NO EVENT SHALL ACTION1 OR ITS SUPPLIERS, OR THEIR RESPECTIVE 
+# OFFICERS, DIRECTORS, EMPLOYEES, OR AGENTS BE LIABLE WITH RESPECT TO THE WEBSITE OR
+# THE COMPONENTS OR THE SERVICES UNDER ANY CONTRACT, NEGLIGENCE, TORT, STRICT 
+# LIABILITY OR OTHER LEGAL OR EQUITABLE THEORY (I)FOR ANY AMOUNT IN THE AGGREGATE IN
+# EXCESS OF THE GREATER OF FEES PAID BY YOU THEREFOR OR $100; (II) FOR ANY INDIRECT,
+# INCIDENTAL, PUNITIVE, OR CONSEQUENTIAL DAMAGES OF ANY KIND WHATSOEVER; (III) FOR
+# DATA LOSS OR COST OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; OR (IV) FOR ANY
+# MATTER BEYOND ACTION1’S REASONABLE CONTROL. SOME STATES DO NOT ALLOW THE
+# EXCLUSION OR LIMITATION OF INCIDENTAL OR CONSEQUENTIAL DAMAGES, SO THE ABOVE
+# LIMITATIONS AND EXCLUSIONS MAY NOT APPLY TO YOU.
 
 # Import the PSAction1 module
 Import-Module PSAction1
