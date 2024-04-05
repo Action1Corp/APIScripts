941d5a0f178da10f69fb311cd3866c0e29cc7251
Sean-Carroll-Action1 <147112281+Sean-Carroll-Action1@users.noreply.github.com>
2024-04-05 12:15:17 -0500

    Update PSAction1DeleteDuplicateEndpoints.ps1

--- PSAction1DeleteDuplicateEndpoints.ps1
+++ PSAction1DeleteDuplicateEndpoints.ps1
@@ -18,9 +18,10 @@
 # LIMITATIONS AND EXCLUSIONS MAY NOT APPLY TO YOU.
 
 # Comment out below import/set-action1credentials if not needed, or preformed prior
-Install-Module -Name PSAction1
-Set-Action1Credentials -APIKey '<Insert API Key Here>' -Secret '<Insert Secret Here>'
-Set-Action1DefaultOrg -Org_ID '<Insert Org_ID here>'
+# Install-Module -Name PSAction1
+# Set-Action1Credentials -APIKey '<Insert API Key Here>' -Secret '<Insert Secret Here>'
+# Set-Action1DefaultOrg -Org_ID '<Insert Org_ID here>'
+# Set-Action1Region -Region '<Enter Region Here>'
 
 $check = @{} # Hash table
 $dupe = New-Object System.Collections.ArrayList # Initialize as an ArrayList
