bc52c950da6e6252fce07dc6a051f60b24ba167f
Aleksy Rembicki <Aleksy.Rembicki.c@action1.com>
2026-04-28 13:48:05 +0200

    Update legal notices. Update headers with legal notices.

--- CustomAttributeUpdateGroupsAllOrgs.ps1
+++ CustomAttributeUpdateGroupsAllOrgs.ps1
@@ -1,7 +1,7 @@
 ﻿# Name: CustomAttributeUpdateGroupsAllOrgs.ps1
 # Description: script is designed to update the Custom Attribute renamed "Groups" with the 
 #   groups each endpoint is a member of. By default, this script will go through all of the organizations in 
-#   your Enterprise and check for the groups each endpoint is a memnber of.  If you only want one organization 
+#   your Enterprise and check for the groups each endpoint is a member of.  If you only want one organization 
 #   checked, create API credentials for that one organization and use those in script below.
 #
 # To use this script,  please edit the script and update the following:
@@ -15,23 +15,18 @@
 # In Action1 console, go to Advanced->Endpoint Custom Attributes
 #   1) Update one of the Custom Attribute names to match "Groups" Spelling, Capitalization and Spacing count
 #
-# Copyright (C) 2024 Action1 Corporation
+
 # Documentation: https://github.com/Action1Corp/PSAction1/
 # Use Action1 Roadmap system (https://roadmap.action1.com/) to submit feedback or enhancement requests.
 
 # WARNING: Carefully study the provided scripts and components before using them. Test in your non-production lab first.
 
-# LIMITATION OF LIABILITY. IN NO EVENT SHALL ACTION1 OR ITS SUPPLIERS, OR THEIR RESPECTIVE 
-# OFFICERS, DIRECTORS, EMPLOYEES, OR AGENTS BE LIABLE WITH RESPECT TO THE WEBSITE OR
-# THE COMPONENTS OR THE SERVICES UNDER ANY CONTRACT, NEGLIGENCE, TORT, STRICT 
-# LIABILITY OR OTHER LEGAL OR EQUITABLE THEORY (I)FOR ANY AMOUNT IN THE AGGREGATE IN
-# EXCESS OF THE GREATER OF FEES PAID BY YOU THEREFOR OR $100; (II) FOR ANY INDIRECT,
-# INCIDENTAL, PUNITIVE, OR CONSEQUENTIAL DAMAGES OF ANY KIND WHATSOEVER; (III) FOR
-# DATA LOSS OR COST OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; OR (IV) FOR ANY
-# MATTER BEYOND ACTION1’S REASONABLE CONTROL. SOME STATES DO NOT ALLOW THE
-# EXCLUSION OR LIMITATION OF INCIDENTAL OR CONSEQUENTIAL DAMAGES, SO THE ABOVE
-# LIMITATIONS AND EXCLUSIONS MAY NOT APPLY TO YOU.
-
+# Action1 Public Repository Material
+# Subject to TERMS_OF_USE.md
+# Provided AS IS
+# Use at your own risk
+# Review and test before production deployment
+# © Action1 Corporation
 
 # NOTE:   Uncomment following try/catch lines if Action1 Powershell Module is not loaded
 
